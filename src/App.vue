3c2c013ec0be34b02cfb77649f516912eca28886
Dmitriy <dfajl@mail.ru>
2024-04-05 17:29:20 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div @click="increment(counter)" class="hello">{{ counter }}</div>
+</template>
+
+<script lang="ts">
+export default {
+  data() {
+    return {
+      counter: 0
+    }
+  },
+  methods: {
+    increment(param: number): number {
+      console.log(param)
+      this.counter++
+      return param
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.hello {
+  display: block;
+  cursor: pointer;
+  color: green;
+  background: #4c4646;
+  height: 50px;
+  width: 20%;
+  transition: 0.3s linear;
+  margin: 0 auto;
+  &:hover {
+    color: rgb(200, 219, 191);
+    background: #cf9e9e;
+  }
+}
+</style>
