ba278cb192b71211f3dbb2539bb3818e243a6130
Dmitriy <dfajl@mail.ru>
2024-05-30 14:48:24 +0300

    Initialize vitest

---
+++ tests/unit/components/UIInput.spec.js
@@ -0,0 +1,6 @@
+import { it, expect } from 'vitest'
+import UIInput from '../../../src/components/UI/UIInput.vue'
+
+it('UiInput should be defined', () => {
+  expect(UIInput).toBeDefined()
+})
--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import viteTsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), viteTsconfigPaths()],
   css: {
     preprocessorOptions: {
       scss: {}
